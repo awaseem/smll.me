3d2509299a8b70d99f33e855a833e04f2ed41e2d
Ali Waseem <waseema393@gmail.com>
2015-04-18 14:19:33 -0400

    added platform detection, chartjs and other javascript plugins

--- client/templateJS/Home.js
+++ client/templateJS/Home.js
@@ -10,5 +10,40 @@ Template.home.helpers({
     }
 });
 
+Template.home.onRendered(function () {
+    var data = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ]
+    var ctx = $("#myChart").get(0).getContext("2d");
+    var myPieChart = new Chart(ctx).Pie(data);
+
+    Tracker.autorun(function () {
+        var count = Urls.findOne( {urlKey: "VyTCxaI-"});
+        if (count) {
+            console.log(count);
+        }
+        myPieChart.segments[1].value += 10
+        myPieChart.update();
+    });
+
+});
+
 //Meteor.call("", "http://www.facebook.com");
-//Meteor.call("updateUrl", "VyTCxaI-", "Canada", "www.facebook.com");
+//Meteor.call("updateUrl", "VyTCxaI-", "Canada", "testGraph");
